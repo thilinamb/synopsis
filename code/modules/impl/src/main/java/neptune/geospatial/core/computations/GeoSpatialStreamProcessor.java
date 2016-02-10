3c9fe47ff2b2f408a003f4b563b0f78730928543
Thilina Buddhika <thilinamb@gmail.com>
2016-02-09 19:19:44 -0700

    Refactoring the scale out code.

--- code/modules/impl/src/main/java/neptune/geospatial/core/computations/GeoSpatialStreamProcessor.java
+++ code/modules/impl/src/main/java/neptune/geospatial/core/computations/GeoSpatialStreamProcessor.java
@@ -15,8 +15,7 @@ import neptune.geospatial.partitioner.GeoHashPartitioner;
 import org.apache.log4j.Logger;
 
 import java.text.DecimalFormat;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
@@ -29,31 +28,45 @@ import java.util.concurrent.atomic.AtomicBoolean;
 @SuppressWarnings("unused")
 public abstract class GeoSpatialStreamProcessor extends StreamProcessor {
 
+    private class MonitoredPrefix implements Comparable<MonitoredPrefix> {
+        private String prefix;
+        private String streamType;
+        private long messageCount;
+        private double messageRate;
+        private long tsLastUpdated;
+
+        public MonitoredPrefix(String prefix, String streamType) {
+            this.prefix = prefix;
+            this.streamType = streamType;
+        }
+
+        @Override
+        public int compareTo(MonitoredPrefix o) {
+            return (int) (this.messageRate - o.messageRate);
+        }
+    }
+
     private class PendingScaleOutRequests {
+        private String prefix;
+        private String streamId;
 
-        private String streamId;    // the new stream id
-
-        public PendingScaleOutRequests(String streamId) {
+        public PendingScaleOutRequests(String prefix, String streamId) {
+            this.prefix = prefix;
             this.streamId = streamId;
         }
     }
 
     private Logger logger = Logger.getLogger(GeoSpatialStreamProcessor.class.getName());
-    public static final String OUTGOING_STREAM = "out-going";
+    public static final String OUTGOING_STREAM_BASE_ID = "out-going";
     private static final String GEO_HASH_CHAR_SET = "0123456789bcdefghjkmnpqrstuvwxyz";
     public static final int GEO_HASH_LEN_IN_CHARS = 32;
     private static final int INPUT_RATE_UPDATE_INTERVAL = 10 * 1000;
 
     private AtomicBoolean initialized = new AtomicBoolean(false);
-
-    private volatile int counter = 0;
-    private AtomicBoolean scaleOutComplete = new AtomicBoolean(false);
-    private AtomicBoolean scaledOut = new AtomicBoolean(false);
-    private String outGoingStream;
-    private Map<String, PendingScaleOutRequests> pendingScaleOutRequestsMap = new HashMap<>();
-    private Map<String, long[]> messageCountsForSubPrefixesMap = new HashMap<>();
-    private Map<String, double[]> messageRatesForSubPrefixesMap = new HashMap<>();
-    private long lastUpdatedTime;
+    private Set<MonitoredPrefix> monitoredPrefixes = new TreeSet<>();
+    private Map<String, MonitoredPrefix> monitoredPrefixMap = new HashMap<>();
+    private Map<String, String> outGoingStreams = new HashMap<>();
+    private Map<String, PendingScaleOutRequests> pendingScaleOutRequests = new HashMap<>();
 
     @Override
     public final void onEvent(StreamEvent streamEvent) throws StreamingDatasetException {
@@ -65,12 +78,11 @@ public abstract class GeoSpatialStreamProcessor extends StreamProcessor {
                 logger.error("Error retrieving the resource instance.", e);
             }
         }
-        counter++;
         GeoHashIndexedRecord geoHashIndexedRecord = (GeoHashIndexedRecord) streamEvent;
         // preprocess each message
         if (preprocess(geoHashIndexedRecord)) {
             // perform the business logic: do this selectively. Send through the traffic we don't process.
-            onEvent(geoHashIndexedRecord);
+            process(geoHashIndexedRecord);
         }
     }
 
@@ -80,7 +92,7 @@ public abstract class GeoSpatialStreamProcessor extends StreamProcessor {
      *
      * @param event <code>GeoHashIndexedRecord</code> element
      */
-    protected abstract void onEvent(GeoHashIndexedRecord event);
+    protected abstract void process(GeoHashIndexedRecord event);
 
     /**
      * Preprocess every record to extract meta-data such as triggering
@@ -89,46 +101,13 @@ public abstract class GeoSpatialStreamProcessor extends StreamProcessor {
      *
      * @param record <code>GeoHashIndexedRecord</code> element
      */
-    protected boolean preprocess(GeoHashIndexedRecord record) {
+    protected boolean preprocess(GeoHashIndexedRecord record) throws StreamingDatasetException {
         updateIncomingRatesForSubPrefixes(record);
+        String prefix = getPrefix(record);
         boolean processLocally = true;
-        // Scale out logic
-        // this is a temporary trigger to test the scaling out.
-        if (!scaledOut.get() && counter >= 1000000) {
-            try {
-                // FIXME: BEGIN - Need to do it only once
-                this.outGoingStream = getStreamIdentifier(OUTGOING_STREAM);
-                GeoHashPartitioner partitioner = new GeoHashPartitioner();
-                String streamType = record.getClass().getName();
-                declareStream(this.outGoingStream, streamType);
-                // FIXME: END
-                // initialize the meta-data
-                Topic[] topics = deployStream(this.outGoingStream, 1, partitioner);
-
-                TriggerScale triggerMessage = new TriggerScale(getInstanceIdentifier(), this.outGoingStream,
-                        topics[0].toString(), streamType);
-                ManagedResource.getInstance().sendToDeployer(triggerMessage);
-
-                if (logger.isDebugEnabled()) {
-                    logger.debug(String.format("[%s] Sent a trigger scale message to deployer.",
-                            getInstanceIdentifier()));
-                }
-                // this is just to control the scaling to one instance for testing.
-                scaledOut.set(true);
-                // FIXME: Increase the prefix length of the record if needed. May be inside the onEvent method.
-            } catch (StreamingDatasetException | StreamingGraphConfigurationException e) {
-                logger.error("Error creating new stream from the current computation.", e);
-            } catch (NIException e) {
-                logger.error("Error sending a trigger scale message to the deployer. ", e);
-            }
-        } else if (scaleOutComplete.get()) {
-            try {
-                writeToStream(outGoingStream, record);
-                processLocally = false;
-                //System.out.println("forwarding to the next child element.");
-            } catch (StreamingDatasetException e) {
-                e.printStackTrace();
-            }
+        if (outGoingStreams.containsKey(prefix)) {
+            writeToStream(outGoingStreams.get(prefix), record);
+            processLocally = false;
         }
         return processLocally;
     }
@@ -139,48 +118,98 @@ public abstract class GeoSpatialStreamProcessor extends StreamProcessor {
     }
 
     public void handleTriggerScaleAck(TriggerScaleAck ack) {
-        scaleOutComplete.set(true);
+        if (pendingScaleOutRequests.containsKey(ack.getInResponseTo())) {
+            PendingScaleOutRequests pendingReq = pendingScaleOutRequests.remove(ack.getInResponseTo());
+            outGoingStreams.put(pendingReq.prefix, pendingReq.streamId);
+        }
     }
 
     private synchronized void updateIncomingRatesForSubPrefixes(GeoHashIndexedRecord record) {
-        String prefix = record.getGeoHash().substring(0, record.getPrefixLength());
-        long[] messageCountsForSubPrefixes;
-        double[] messageRatesForSubPrefixes;
-        if (messageCountsForSubPrefixesMap.containsKey(prefix)) {
-            messageCountsForSubPrefixes = messageCountsForSubPrefixesMap.get(prefix);
-            messageRatesForSubPrefixes = messageRatesForSubPrefixesMap.get(prefix);
+        String prefix = getPrefix(record);
+        MonitoredPrefix monitoredPrefix;
+        if (monitoredPrefixMap.containsKey(prefix)) {
+            monitoredPrefix = monitoredPrefixMap.get(prefix);
         } else {
-            messageCountsForSubPrefixes = new long[GEO_HASH_LEN_IN_CHARS];
-            messageRatesForSubPrefixes = new double[GEO_HASH_LEN_IN_CHARS];
-            messageCountsForSubPrefixesMap.put(prefix, messageCountsForSubPrefixes);
-            messageRatesForSubPrefixesMap.put(prefix, messageRatesForSubPrefixes);
+            monitoredPrefix = new MonitoredPrefix(prefix, record.getClass().getName());
+            monitoredPrefixes.add(monitoredPrefix);
+            monitoredPrefixMap.put(prefix, monitoredPrefix);
         }
 
-        int index = getIndexForSubPrefix(record.getGeoHash(), record.getPrefixLength());
-        messageCountsForSubPrefixes[index]++;
+        monitoredPrefix.messageCount++;
 
         long timeNow = System.currentTimeMillis();
-        if (lastUpdatedTime == 0) {
-            lastUpdatedTime = timeNow;
-        } else if ((timeNow - lastUpdatedTime) > INPUT_RATE_UPDATE_INTERVAL) {
-            double timeElapsed = (timeNow - lastUpdatedTime) * 1.0;
-            StringBuilder sBuilder = new StringBuilder();
+        if (monitoredPrefix.tsLastUpdated == 0) {
+            monitoredPrefix.tsLastUpdated = timeNow;
+        } else if ((timeNow - monitoredPrefix.tsLastUpdated) > INPUT_RATE_UPDATE_INTERVAL) {
+            double timeElapsed = (timeNow - monitoredPrefix.tsLastUpdated) * 1.0;
             DecimalFormat dFormat = new DecimalFormat();
-            for (int i = 0; i < GEO_HASH_LEN_IN_CHARS; i++) {
-                messageRatesForSubPrefixes[i] = messageCountsForSubPrefixes[i] / timeElapsed;
-                messageCountsForSubPrefixes[i] = 0;
-                sBuilder.append(GEO_HASH_CHAR_SET.charAt(i)).append(" - ").append(
-                        dFormat.format(messageRatesForSubPrefixes[i])).append(", ");
+            monitoredPrefix.messageRate = monitoredPrefix.messageCount * 1000.0 / timeElapsed;
+            monitoredPrefix.messageCount = 0;
+            if (logger.isDebugEnabled()) {
+                logger.debug(String.format("Prefix: %s, Message Rate: %.3f", prefix, monitoredPrefix.messageRate));
             }
-            System.out.println(sBuilder);
-            lastUpdatedTime = timeNow;
+            monitoredPrefix.tsLastUpdated = timeNow;
         }
+    }
+
+    private String getPrefix(GeoHashIndexedRecord record) {
+        return record.getGeoHash().substring(0, record.getPrefixLength() + 1);
     }
 
     private int getIndexForSubPrefix(String geohash, int prefixLength) {
         return GEO_HASH_CHAR_SET.indexOf(geohash.charAt(prefixLength));
     }
 
+    public long getBacklogLength() {
+        return streamDataset.getQueueLengthInBytes();
+    }
+
+    /**
+     * Resource recommends scaling out for one or more prefixes.
+     */
+    public void recommendScaleOut(double excess) {
+        List<String> prefixesForScalingOut = new ArrayList<>();
+        double cumulSumOfPrefixes = 0;
+        synchronized (this) {
+            Iterator<MonitoredPrefix> itr = monitoredPrefixes.iterator();
+            while (itr.hasNext() && cumulSumOfPrefixes < excess) {
+                MonitoredPrefix monitoredPrefix = itr.next();
+                prefixesForScalingOut.add(monitoredPrefix.prefix);
+                cumulSumOfPrefixes += monitoredPrefix.messageRate;
+            }
+        }
+        for (String prefix : prefixesForScalingOut) {
+            String streamType = monitoredPrefixMap.get(prefix).streamType;
+            initiateScaleOut(prefix, streamType);
+        }
+    }
+
+    public void initiateScaleOut(String prefix, String streamType) {
+        try {
+            GeoHashPartitioner partitioner = new GeoHashPartitioner();
+            String outGoingStreamId = getNewStreamIdentifier(prefix);
+            declareStream(outGoingStreamId, streamType);
+            // initialize the meta-data
+            Topic[] topics = deployStream(outGoingStreamId, 1, partitioner);
+
+            TriggerScale triggerMessage = new TriggerScale(getInstanceIdentifier(), outGoingStreamId,
+                    topics[0].toString(), streamType);
+            pendingScaleOutRequests.put(triggerMessage.getMessageId(), new PendingScaleOutRequests(prefix, outGoingStreamId));
+            ManagedResource.getInstance().sendToDeployer(triggerMessage);
+            if (logger.isDebugEnabled()) {
+                logger.debug(String.format("[%s] Sent a trigger scale message to deployer.",
+                        getInstanceIdentifier()));
+            }
+        } catch (StreamingDatasetException | StreamingGraphConfigurationException e) {
+            logger.error("Error creating new stream from the current computation.", e);
+        } catch (NIException e) {
+            logger.error("Error sending a trigger scale message to the deployer. ", e);
+        }
+    }
+
+    private String getNewStreamIdentifier(String prefix) {
+        return getStreamIdentifier(OUTGOING_STREAM_BASE_ID + "-" + prefix);
+    }
 }
 
 
--- code/modules/impl/src/main/java/neptune/geospatial/core/resource/ManagedResource.java
+++ code/modules/impl/src/main/java/neptune/geospatial/core/resource/ManagedResource.java
@@ -18,10 +18,12 @@ import org.apache.log4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
+import java.util.*;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Improves the behavior of <code>Resource</code> by
@@ -33,6 +35,40 @@ import java.util.concurrent.CountDownLatch;
  */
 public class ManagedResource {
 
+    class MonitoredComputationState {
+        private GeoSpatialStreamProcessor computation;
+        private List<Long> backLogHistory = new ArrayList<>(MONITORED_BACKLOG_HISTORY_LENGTH);
+        private AtomicBoolean eligibleForScaleOut = new AtomicBoolean(true);
+
+        private MonitoredComputationState(GeoSpatialStreamProcessor computation) {
+            this.computation = computation;
+        }
+
+        private boolean monitor() {
+            long currentBacklog = computation.getBacklogLength();
+            backLogHistory.add(currentBacklog);
+            while (backLogHistory.size() > MONITORED_BACKLOG_HISTORY_LENGTH) {
+                backLogHistory.remove(0);
+            }
+            return isBacklogDeveloping();
+        }
+
+        private boolean isBacklogDeveloping() {
+            if (backLogHistory.size() >= MONITORED_BACKLOG_HISTORY_LENGTH) {
+                boolean isDeveloping = true;
+                for (int i = 0; i < backLogHistory.size(); i++) {
+                    if (backLogHistory.get(i) < THRESHOLD) {
+                        isDeveloping = false;
+                        break;
+                    }
+                }
+                return isDeveloping;
+            } else {
+                return false;
+            }
+        }
+    }
+
     /**
      * Monitors the computations to detect the stragglers.
      */
@@ -40,25 +76,36 @@ public class ManagedResource {
         @Override
         public void run() {
             try {
-                synchronized (ManagedResource.this){
-                    if(streamProcessors.isEmpty()){
-                        streamProcessors.wait();
+                synchronized (ManagedResource.this) {
+                    // wait till computations are registered. avoid busy waiting at the beginning.
+                    if (monitoredProcessors.isEmpty()) {
+                        monitoredProcessors.wait();
                     }
-                    
+                    for (String identifier : monitoredProcessors.keySet()) {
+                        MonitoredComputationState monitoredComputationState = monitoredProcessors.get(identifier);
+                        if (monitoredComputationState.eligibleForScaleOut.get() && monitoredComputationState.monitor()) {
+                            // trigger scale up
+                            monitoredComputationState.eligibleForScaleOut.set(true);
+                        }
+                    }
                 }
             } catch (Throwable e) {
-
+                logger.error("Error in running computation monitor task.", e);
             }
         }
     }
 
 
     private static Logger logger = Logger.getLogger(ManagedResource.class.getName());
+    public static final int MONITORED_BACKLOG_HISTORY_LENGTH = 5;
+    public static final long THRESHOLD = 5 * 1024;
+    public static final int MONITORING_PERIOD = 10 * 1000;
 
     private static ManagedResource instance;
     private String deployerEndpoint = null;
     private CountDownLatch countDownLatch = new CountDownLatch(1);
-    private Map<String, GeoSpatialStreamProcessor> streamProcessors = new HashMap<>();
+    private Map<String, MonitoredComputationState> monitoredProcessors = new HashMap<>();
+    private ScheduledExecutorService monitoringService = Executors.newSingleThreadScheduledExecutor();
 
     public ManagedResource(Properties inProps, int numOfThreads) throws CommunicationsException {
         Resource resource = new Resource(inProps, numOfThreads);
@@ -72,6 +119,8 @@ public class ManagedResource {
         AbstractProtocolHandler protoHandler = new ResourceProtocolHandler(this);
         ControlMessageDispatcher.getInstance().registerCallback(Constants.WILD_CARD_CALLBACK, protoHandler);
         new Thread(protoHandler).start();
+        // start the computation monitor thread
+        monitoringService.scheduleWithFixedDelay(new ComputationMonitor(), 0, MONITORING_PERIOD, TimeUnit.MILLISECONDS);
         try {
             deployerEndpoint = NeptuneRuntime.getInstance().getProperties().getProperty(
                     Constants.DEPLOYER_ENDPOINT);
@@ -148,15 +197,19 @@ public class ManagedResource {
     }
 
     public synchronized void registerStreamProcessor(GeoSpatialStreamProcessor processor) {
-        streamProcessors.put(processor.getInstanceIdentifier(), processor);
-        streamProcessors.notifyAll();
+        monitoredProcessors.put(processor.getInstanceIdentifier(), new MonitoredComputationState(processor));
+        monitoredProcessors.notifyAll();
     }
 
+    public void scaleOutComplete(String computationIdentifier) {
+        MonitoredComputationState monitoredComputationState = monitoredProcessors.get(computationIdentifier);
+        monitoredComputationState.eligibleForScaleOut.set(true);
+    }
 
     public void handleTriggerScaleAck(TriggerScaleAck ack) {
         String processorId = ack.getTargetComputation();
-        if (streamProcessors.containsKey(processorId)) {
-            streamProcessors.get(processorId).handleTriggerScaleAck(ack);
+        if (monitoredProcessors.containsKey(processorId)) {
+            monitoredProcessors.get(processorId).computation.handleTriggerScaleAck(ack);
         } else {
             logger.warn("ScaleTriggerAck for an invalid computation: " + processorId);
         }
