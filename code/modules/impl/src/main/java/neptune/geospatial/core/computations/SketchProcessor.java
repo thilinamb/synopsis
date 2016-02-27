53e8dc52488f67fa72b0fdfd4ae7143257795900
Matthew Malensek <matt@malensek.net>
2016-02-26 18:59:05 -0700

    Start adding Sketch processor implementation

--- code/modules/impl/src/main/java/neptune/geospatial/core/computations/SketchProcessor.java
+++ code/modules/impl/src/main/java/neptune/geospatial/core/computations/SketchProcessor.java
@@ -1,11 +1,44 @@
 package neptune.geospatial.core.computations;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import io.sigpipe.sing.dataset.Metadata;
+import io.sigpipe.sing.graph.Path;
+import io.sigpipe.sing.graph.Sketch;
+import io.sigpipe.sing.serialization.Serializer;
+
 import neptune.geospatial.graph.messages.GeoHashIndexedRecord;
 
 public class SketchProcessor extends AbstractGeoSpatialStreamProcessor {
 
+    private Map<String, Sketch> sketches = new HashMap<>();
+
     protected void process(GeoHashIndexedRecord event) {
 
+        String location = event.getGeoHash();
+        Sketch sketch = sketches.get(location);
+        if (sketch == null) {
+            sketch = new Sketch();
+            sketches.put(location, sketch);
+        }
+
+        Metadata eventMetadata;
+        try {
+            byte[] payload = event.getPayload();
+            eventMetadata = Serializer.deserialize(Metadata.class, payload);
+        } catch (IOException e) {
+            //TODO log this
+            System.out.println("Could not deserialize event payload");
+            e.printStackTrace();
+        }
+
+        // TODO: preprocessing of incoming metadata goes here. remove features,
+        // etc.
+        Path path = new Path(eventMetadata.getAttributes().toArray());
+
+        sketch.addPath(path);
     }
 
     public byte[] split(String prefix) {
