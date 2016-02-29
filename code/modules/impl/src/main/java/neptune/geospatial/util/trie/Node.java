90ae55cc0f7dfc981f7d396c9712501088c47cc4
Thilina Buddhika <thilinamb@gmail.com>
2016-02-28 20:01:12 -0700

    - Adding test code to the Node class of the prefix tree. - Fixing minor issues.

--- code/modules/impl/src/main/java/neptune/geospatial/util/trie/Node.java
+++ code/modules/impl/src/main/java/neptune/geospatial/util/trie/Node.java
@@ -3,8 +3,7 @@ package neptune.geospatial.util.trie;
 
 import org.apache.log4j.Logger;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author Thilina Buddhika
@@ -43,51 +42,93 @@ public class Node {
      */
     private String ctrlEndpoint;
 
-    public Node(String prefix) {
+    public Node(String prefix, String computationId, String ctrlEndpoint) {
         this.prefix = prefix;
         this.prefixLength = prefix.length();
+        this.computationId = computationId;
+        this.ctrlEndpoint = ctrlEndpoint;
     }
 
-    // void insert(Node node, String prefix), Node search(string prefix)
+    /**
+     * Registers a new prefix in an existing node.
+     * @param node  A node representing the given sub-prefix
+     */
     public void add(Node node) {
-        String newNodePrefix = node.prefix;
-        String childQualifier = newNodePrefix.substring(prefixLength, prefixLength + CHILD_NODE_QUALIFIER_LENGTH);
-        if (childNodes.containsKey(childQualifier)) { // there is a child node who can handles a longer prefix.
-            childNodes.get(childQualifier).add(node);
-        } else { // it should be a prefix that the current node maintains.
-            if (this.computationId.equals(node.computationId) && (this.ctrlEndpoint.equals(node.ctrlEndpoint))) {
-                if (logger.isDebugEnabled()) {
-                    logger.debug(String.format("[Trie: %s] New prefix %s added to %s", prefix, newNodePrefix,
-                            computationId));
+        if (prefixLength > 0) {
+            String newNodePrefix = node.prefix;
+            String childQualifier = newNodePrefix.substring(prefixLength, prefixLength + CHILD_NODE_QUALIFIER_LENGTH);
+            if (childNodes.containsKey(childQualifier)) { // there is a child node who can handles a longer prefix.
+                childNodes.get(childQualifier).add(node);
+            } else { // it should be a prefix that the current node maintains.
+                if (this.computationId.equals(node.computationId)) {
+                    if (logger.isDebugEnabled()) {
+                        logger.debug(String.format("[Trie: %s] New prefix %s added to %s", prefix, newNodePrefix,
+                                computationId));
+                    }
                 } else {
                     logger.error(String.format("[Trie: %s] Error in the trie. Conflicting endpoints for the prefix. " +
                                     "Prefix: %s, Provided Comp: %s, Expected Comp: %s", prefix, newNodePrefix,
                             node.computationId, computationId));
                 }
             }
+        } else {
+            expand(node, false);
         }
     }
 
+    /**
+     * Scales out a given prefix
+     * @param node  A node representing the given sub-prefix
+     */
     public void expand(Node node) {
+        expand(node, true);
+    }
+
+    /**
+     * Scales out a given prefix
+     * @param node A node representing the given sub-prefix
+     * @param recursive Whether to perform a recursive expansion.
+     *                  Used to handles a special case when expanding from the root.
+     */
+    public void expand(Node node, boolean recursive) {
         String newNodePrefix = node.prefix;
         String childQualifier = newNodePrefix.substring(prefixLength, prefixLength + CHILD_NODE_QUALIFIER_LENGTH);
         if (childNodes.containsKey(childQualifier)) { // there is a child node who can handles a longer prefix.
-            childNodes.get(childQualifier).expand(node);
+            if (recursive) {
+                childNodes.get(childQualifier).expand(node);
+            } else {
+                childNodes.get(childQualifier).add(node);
+            }
         } else {
             childNodes.put(childQualifier, node);
-            if(logger.isDebugEnabled()){
+            if (logger.isDebugEnabled()) {
                 logger.debug(String.format("[Trie - %s] Trie is expanded. A new child node added. New prefix: %s, " +
                         "New Comp: %s", prefix, newNodePrefix, node.computationId));
             }
         }
     }
 
+    /**
+     * Scales in the given sub-prefix.
+     * @param node A node representing the given sub-prefix.
+     */
     public void shrink(Node node) {
         String newNodePrefix = node.prefix;
         String childQualifier = newNodePrefix.substring(prefixLength, prefixLength + CHILD_NODE_QUALIFIER_LENGTH);
         if (childNodes.containsKey(childQualifier)) { // there is a child node who can handles a longer prefix.
             if (childNodes.get(childQualifier).prefixLength == newNodePrefix.length()) {
                 childNodes.remove(childQualifier);
+                if (this.computationId.equals(node.computationId)) {
+                    if (logger.isDebugEnabled()) {
+                        logger.debug(String.format("[Trie: %s] Prefix %s scaled in to %s", prefix, newNodePrefix,
+                                computationId));
+                    }
+                } else {
+                    logger.error(String.format("[Trie: %s] Error in the trie. Conflicting endpoints for the prefix. " +
+                                    "Prefix: %s, Provided Comp: %s, Expected Comp: %s", prefix, newNodePrefix,
+                            node.computationId, computationId));
+                }
+
             } else {
                 childNodes.get(childQualifier).shrink(node);
             }
@@ -97,8 +138,77 @@ public class Node {
         }
     }
 
-    public static void main(String[] args) {
+    /**
+     * Traverse the prefix tree in a depth first manner and return the list of prefixes.
+     * @return a list of prefixes. Each node returns a sorted list of child prefixes followed by its own prefix
+     */
+    private List<String> traverse() {
+        // first sort the children
+        TreeSet<String> sortedKeys = new TreeSet<>(childNodes.keySet());
+        List<String> traverseResults = new ArrayList<>();
+        // for each child, call traverse and concatenate their list.
+        for (String key : sortedKeys) {
+            List<String> childTraverseResults = childNodes.get(key).traverse();
+            for (String childTraverseResult : childTraverseResults) {
+                childTraverseResult += "(" + this.prefix + ")";
+                traverseResults.add(childTraverseResult);
+            }
+        }
+        traverseResults.add(prefix);
+        return traverseResults;
+    }
 
+    /**
+     * Encodes the results of a traverse into a single String
+     * @param traverseResults List of prefixes returned by the {@code traverse} method.
+     * @return A string encoded version of the prefix results.
+     */
+    private String printTraverseResults(List<String> traverseResults) {
+        StringBuilder sBuilder = new StringBuilder();
+        for (String prefix : traverseResults) {
+            if (prefix.equals("")) {
+                sBuilder.append("_");
+            } else {
+                sBuilder.append(prefix);
+            }
+            sBuilder.append(":");
+        }
+        sBuilder.deleteCharAt(sBuilder.lastIndexOf(":"));
+        return sBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node("", "comp_id", "localhost:9099");
+        System.out.println(root.printTraverseResults(root.traverse()));
+        root.add(new Node("9X", "comp_id-1", "localhost:9099"));
+        System.out.println(root.printTraverseResults(root.traverse()));
+        root.add(new Node("8G", "comp_id-2", "localhost:9099"));
+        System.out.println(root.printTraverseResults(root.traverse()));
+        root.add(new Node("A", "comp_id-3", "localhost:9099"));
+        System.out.println(root.printTraverseResults(root.traverse()));
+
+        System.out.println("----------------------");
+        root.expand(new Node("9X1", "comp_id-4", "localhost:9099"));
+        root.expand(new Node("9X2", "comp_id-5", "localhost:9099"));
+        root.expand(new Node("9X1A", "comp_id-6", "localhost:9099"));
+        root.expand(new Node("9X3", "comp_id-7", "localhost:9099"));
+        System.out.println(root.printTraverseResults(root.traverse()));
+
+        System.out.println("----------------------");
+        root.shrink(new Node("9X1", "comp_id-1", "localhost:9099"));
+        System.out.println(root.printTraverseResults(root.traverse()));
+
+        System.out.println("----------------------");
+        root.add(new Node("9X1", "comp_id-1", "localhost:9099"));
+        System.out.println(root.printTraverseResults(root.traverse()));
+
+        System.out.println("----------------------");
+        root.expand(new Node("9X1", "comp_id-4", "localhost:9099"));
+        System.out.println(root.printTraverseResults(root.traverse()));
+
+        System.out.println("----------------------");
+        root.expand(new Node("9X1A", "comp_id-5", "localhost:9099"));
+        System.out.println(root.printTraverseResults(root.traverse()));
     }
 
 }
