27c23c95a7cd5f457efadfd8181c00ce722e8b9e
Thilina Buddhika <thilinamb@gmail.com>
2016-03-01 17:37:16 -0700

    Making the script generic. Reading the current username instead of hard coding.

--- code/bin/kill-resource.sh
+++ code/bin/kill-resource.sh
@@ -3,7 +3,7 @@
 r_pid=`jps | grep 'ManagedResource' | cut -d' ' -f1`
 echo 'Resource PID: '${r_pid}
 if [ -z ${r_pid} ]; then 
-    r_pid=`ps -ef | grep 'java' | grep 'thilinab' | grep 'granules' | cut -d' ' -f2`
+    r_pid=`ps -ef | grep 'java' | grep ${USER} | grep 'granules' | cut -d' ' -f2`
     echo '[PS] Resource PID: '${r_pid}
 fi
 
