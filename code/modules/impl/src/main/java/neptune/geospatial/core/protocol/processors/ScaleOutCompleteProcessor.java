df2d2ee95e5812750d0c93c7bc39088514d192da
Thilina Buddhika <thilinamb@gmail.com>
2016-03-17 13:21:38 -0600

    Adding protocol processors for scale out protocol messages.

---
+++ code/modules/impl/src/main/java/neptune/geospatial/core/protocol/processors/ScaleOutCompleteProcessor.java
@@ -0,0 +1,58 @@
+package neptune.geospatial.core.protocol.processors;
+
+import com.hazelcast.core.IQueue;
+import ds.granules.communication.direct.control.ControlMessage;
+import ds.granules.neptune.interfere.core.NIException;
+import neptune.geospatial.core.computations.AbstractGeoSpatialStreamProcessor;
+import neptune.geospatial.core.computations.scalingctxt.PendingScaleOutRequest;
+import neptune.geospatial.core.computations.scalingctxt.ScalingContext;
+import neptune.geospatial.core.protocol.msg.ScaleOutCompleteAck;
+import neptune.geospatial.core.resource.ManagedResource;
+import org.apache.log4j.Logger;
+
+/**
+ * @author Thilina Buddhika
+ */
+public class ScaleOutCompleteProcessor implements ProtocolProcessor {
+
+    private Logger logger = Logger.getLogger(ScaleOutCompleteProcessor.class);
+
+    @Override
+    public void process(ControlMessage ctrlMsg, ScalingContext scalingContext,
+                        AbstractGeoSpatialStreamProcessor streamProcessor) {
+        ScaleOutCompleteAck ack = (ScaleOutCompleteAck) ctrlMsg;
+        PendingScaleOutRequest pendingReq = scalingContext.getPendingScaleOutRequest(ack.getKey());
+        String instanceIdentifier = streamProcessor.getInstanceIdentifier();
+        if (pendingReq != null) {
+            int ackCount = pendingReq.incrementAndGetAckCount();
+            if (logger.isDebugEnabled()) {
+                logger.debug(String.format("[%s] Received a ScaleOutCompleteAck. Sent Count: %d, Received Count: %d",
+                        instanceIdentifier, pendingReq.getPrefixes().size(), ackCount));
+            }
+            // update prefix tree
+            /*IMap<String, SketchLocation> prefMap = hzInstance.getMap(GeoHashPrefixTree.PREFIX_MAP);
+            MonitoredPrefix monitoredPrefix = monitoredPrefixMap.get(ack.getPrefix());
+            prefMap.put(ack.getPrefix(), new SketchLocation(monitoredPrefix.destComputationId,
+                    monitoredPrefix.destResourceCtrlEndpoint,
+                    SketchLocation.MODE_SCALE_OUT));
+            */
+            if (ackCount == pendingReq.getPrefixes().size()) {
+                try {
+                    if (logger.isDebugEnabled()) {
+                        logger.debug(String.format("[%s] Scaling out complete for now.", instanceIdentifier));
+                    }
+                    // TODO: temporary fix to track dynamic scaling. Remove this and uncomment above after the micro-benchmark
+                    IQueue<Integer> scalingMonitoringQueue = streamProcessor.getHzInstance().getQueue("scaling-monitor");
+                    scalingMonitoringQueue.add(ackCount);
+                    scalingContext.completeScalingOut(ack.getKey());
+                    streamProcessor.releaseMutex();
+                    ManagedResource.getInstance().scalingOperationComplete(instanceIdentifier);
+                } catch (NIException ignore) {
+
+                }
+            }
+        } else {
+            logger.warn("Invalid ScaleOutCompleteAck for " + ack.getKey());
+        }
+    }
+}
---
+++ code/modules/impl/src/main/java/neptune/geospatial/core/protocol/processors/ScaleOutResponseProcessor.java
@@ -0,0 +1,66 @@
+package neptune.geospatial.core.protocol.processors;
+
+import ds.granules.communication.direct.control.ControlMessage;
+import ds.granules.communication.direct.control.SendUtility;
+import ds.granules.exception.CommunicationsException;
+import ds.granules.streaming.core.exception.StreamingDatasetException;
+import neptune.geospatial.core.computations.AbstractGeoSpatialStreamProcessor;
+import neptune.geospatial.core.computations.scalingctxt.MonitoredPrefix;
+import neptune.geospatial.core.computations.scalingctxt.PendingScaleOutRequest;
+import neptune.geospatial.core.computations.scalingctxt.ScalingContext;
+import neptune.geospatial.core.protocol.msg.ScaleOutResponse;
+import neptune.geospatial.core.protocol.msg.StateTransferMsg;
+import neptune.geospatial.graph.messages.GeoHashIndexedRecord;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+
+/**
+ * Handles {@code ScaleOutResponse} from the deployer
+ *
+ * @author Thilina Buddhika
+ */
+public class ScaleOutResponseProcessor implements ProtocolProcessor{
+
+    private Logger logger = Logger.getLogger(ScaleOutResponseProcessor.class);
+
+    @Override
+    public void process(ControlMessage ctrlMessage, ScalingContext scalingContext,
+                        AbstractGeoSpatialStreamProcessor streamProcessor) {
+        ScaleOutResponse scaleOutResp = (ScaleOutResponse)ctrlMessage;
+        PendingScaleOutRequest pendingReq = scalingContext.getPendingScaleOutRequest(scaleOutResp.getInResponseTo());
+        String instanceIdentifier = streamProcessor.getInstanceIdentifier();
+        if (pendingReq != null) {
+            for (String prefix : pendingReq.getPrefixes()) {
+                MonitoredPrefix monitoredPrefix = scalingContext.getMonitoredPrefix(prefix);
+                monitoredPrefix.setIsPassThroughTraffic(true);
+                monitoredPrefix.setDestComputationId(scaleOutResp.getNewComputationId());
+                monitoredPrefix.setDestResourceCtrlEndpoint(scaleOutResp.getNewLocationURL());
+                monitoredPrefix.setOutGoingStream(pendingReq.getStreamId());
+                try {
+                    // send a dummy message, just to ensure the new computation is activated.
+                    GeoHashIndexedRecord record = new GeoHashIndexedRecord(monitoredPrefix.getLastGeoHashSent(),
+                            prefix.length() + 1, -1, System.currentTimeMillis(), new byte[0]);
+                    streamProcessor.emit(monitoredPrefix.getOutGoingStream(), record);
+                    byte[] state = streamProcessor.split(prefix);
+                    StateTransferMsg stateTransferMsg = new StateTransferMsg(prefix, scaleOutResp.getInResponseTo(),
+                            state, scaleOutResp.getNewComputationId(), instanceIdentifier,
+                            StateTransferMsg.SCALE_OUT);
+                    stateTransferMsg.setLastMessageId(monitoredPrefix.getLastMessageSent());
+                    stateTransferMsg.setLastMessagePrefix(monitoredPrefix.getLastGeoHashSent());
+                    SendUtility.sendControlMessage(monitoredPrefix.getDestResourceCtrlEndpoint(), stateTransferMsg);
+                    if (logger.isDebugEnabled()) {
+                        logger.debug(String.format("[%s] New Pass-Thru Prefix. Prefix: %s, Outgoing Stream: %s",
+                                instanceIdentifier, prefix, pendingReq.getStreamId()));
+                    }
+                } catch (CommunicationsException | IOException e) {
+                    logger.error("Error transferring state to " + monitoredPrefix.getDestResourceCtrlEndpoint());
+                } catch (StreamingDatasetException e) {
+                    logger.error("Error sending a message");
+                }
+            }
+        } else {
+            logger.warn("Invalid trigger scaleOutResp for the prefix. Request Id: " + scaleOutResp.getInResponseTo());
+        }
+    }
+}
