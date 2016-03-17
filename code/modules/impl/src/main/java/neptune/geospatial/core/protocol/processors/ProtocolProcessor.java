4a9b29b552974214e548c5f031cb68f080cfbeb2
Thilina Buddhika <thilinamb@gmail.com>
2016-03-17 13:20:50 -0600

    Adding the protocol processor interface which is implemented by individual protocol message handlers.

---
+++ code/modules/impl/src/main/java/neptune/geospatial/core/protocol/processors/ProtocolProcessor.java
@@ -0,0 +1,24 @@
+package neptune.geospatial.core.protocol.processors;
+
+import ds.granules.communication.direct.control.ControlMessage;
+import neptune.geospatial.core.computations.AbstractGeoSpatialStreamProcessor;
+import neptune.geospatial.core.computations.scalingctxt.ScalingContext;
+
+/**
+ * Processes a specific control message in the protocol flow for dynamic scaling
+ *
+ * @author Thilina Buddhika
+ */
+public interface ProtocolProcessor {
+
+    /**
+     * Processes a control message
+     * 
+     * @param ctrlMsg {@link ds.granules.communication.direct.control.ControlMessage} itself
+     * @param scalingContext Instance of the {@link neptune.geospatial.core.computations.scalingctxt.ScalingContext} corresponding to the computation
+     * @param streamProcessor {@link neptune.geospatial.core.computations.AbstractGeoSpatialStreamProcessor} object
+     */
+    public void process(ControlMessage ctrlMsg, ScalingContext scalingContext,
+                        AbstractGeoSpatialStreamProcessor streamProcessor);
+
+}
