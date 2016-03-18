a9b9181127d97e219b1b0722b722de0d50b74bc1
Thilina Buddhika <thilinamb@gmail.com>
2016-03-18 13:12:15 -0600

    Moving out the benchmark related code from core. Using the new API methods to record scaling activities.

--- code/modules/benchmarks/src/main/java/neptune/geospatial/benchmarks/dynamicscaling/StreamProcessor.java
+++ code/modules/benchmarks/src/main/java/neptune/geospatial/benchmarks/dynamicscaling/StreamProcessor.java
@@ -1,7 +1,10 @@
 package neptune.geospatial.benchmarks.dynamicscaling;
 
+import com.hazelcast.core.IQueue;
 import neptune.geospatial.core.computations.AbstractGeoSpatialStreamProcessor;
 import neptune.geospatial.graph.messages.GeoHashIndexedRecord;
+
+import java.util.List;
 
 /**
  * @author Thilina Buddhika
@@ -25,5 +28,17 @@ public class StreamProcessor extends AbstractGeoSpatialStreamProcessor {
     @Override
     public void merge(String prefix, byte[] serializedSketch) {
 
+    }
+
+    @Override
+    public void onSuccessfulScaleOut(List<String> prefixes) {
+        IQueue<Integer> scalingMonitoringQueue = getHzInstance().getQueue("scaling-monitor");
+        scalingMonitoringQueue.add(prefixes.size());
+    }
+
+    @Override
+    public void onSuccessfulScaleIn(List<String> prefixes) {
+        IQueue<Integer> scaleMonitorQueue = getHzInstance().getQueue("scaling-monitor");
+        scaleMonitorQueue.add(-1 * prefixes.size());
     }
 }
