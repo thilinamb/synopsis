98d2a5015cbbd94ad69f420a60da4b76c6bcffb3
Matthew Malensek <matt@malensek.net>
2016-03-19 00:02:48 -0600

    Add rough quantization benchmark
    This still needs an error plot / formatting updates.

---
+++ benchmarks/sketch-quant/temperature/plot.py
@@ -0,0 +1,32 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats as stats
+
+from matplotlib import rc
+rc('font',**{'family':'sans-serif','sans-serif':['Arial']})
+
+temps = np.loadtxt(fname='./all_temps.txt.bz2')
+ticks = np.loadtxt(fname='./test4')
+
+kernel = stats.kde.gaussian_kde(temps)
+kernel.covariance_factor = lambda : .20
+kernel._compute_covariance()
+
+plt.ion()
+plt.clf()
+
+plt.suptitle('Synopsis Creation: Surface Temperature', fontsize=16)
+plt.xlabel('Temperature (Kelvin)', fontsize=14)
+plt.ylabel('Probability Density', fontsize=14)
+plt.axis([237, 320, 0, 0.045])
+
+x = np.array(range(int(temps.min()), int(temps.max()) + 1))
+plt.fill_between(x, 0, kernel(x), color='#82c6f5', alpha=0.3)
+for tick in ticks[1:, 0]:
+    plt.plot([tick, tick], [0, kernel(tick)], color='#2d7fb7', alpha=0.75)
+plt.plot(x, kernel(x), color='#5aaae2', lw=4)
+
+#TODO: error plot below main PDF
+
+#plt.savefig('../../paper/figures/quantization.pdf', bbox_inches='tight')
