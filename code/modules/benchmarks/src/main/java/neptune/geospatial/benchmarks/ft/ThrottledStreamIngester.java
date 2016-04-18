ef6e29a7c846ab7c2cc0e23a5e5ee547a131fb48
Thilina Buddhika <thilinamb@gmail.com>
2016-04-18 17:02:30 -0600

    Emitting periodic checkpointing requests

--- code/modules/benchmarks/src/main/java/neptune/geospatial/benchmarks/ft/ThrottledStreamIngester.java
+++ code/modules/benchmarks/src/main/java/neptune/geospatial/benchmarks/ft/ThrottledStreamIngester.java
@@ -2,13 +2,16 @@ package neptune.geospatial.benchmarks.ft;
 
 import com.hazelcast.core.IQueue;
 import ds.funnel.data.format.FormatReader;
+import ds.granules.communication.direct.control.ControlMessage;
 import ds.granules.exception.CommunicationsException;
+import ds.granules.exception.GranulesConfigurationException;
 import ds.granules.neptune.interfere.core.NIException;
 import ds.granules.streaming.core.exception.StreamingDatasetException;
 import neptune.geospatial.benchmarks.util.SineCurveLoadProfiler;
 import neptune.geospatial.core.resource.ManagedResource;
 import neptune.geospatial.ft.BackupTopicInfo;
 import neptune.geospatial.ft.FaultTolerantStreamBase;
+import neptune.geospatial.ft.protocol.CheckpointAck;
 import neptune.geospatial.ft.zk.MembershipChangeListener;
 import neptune.geospatial.ft.zk.MembershipTracker;
 import neptune.geospatial.graph.Constants;
@@ -16,6 +19,7 @@ import neptune.geospatial.graph.messages.GeoHashIndexedRecord;
 import neptune.geospatial.graph.operators.NOAADataIngester;
 import neptune.geospatial.hazelcast.HazelcastClientInstanceHolder;
 import neptune.geospatial.hazelcast.HazelcastException;
+import neptune.geospatial.util.RivuletUtil;
 import org.apache.log4j.Logger;
 
 import java.io.BufferedWriter;
@@ -29,7 +33,7 @@ import java.util.concurrent.atomic.AtomicLong;
 /**
  * @author Thilina Buddhika
  */
-class ThrottledStreamIngester extends NOAADataIngester implements FaultTolerantStreamBase, MembershipChangeListener {
+public class ThrottledStreamIngester extends NOAADataIngester implements FaultTolerantStreamBase, MembershipChangeListener {
 
     private Logger logger = Logger.getLogger(ThrottledStreamIngester.class);
 
@@ -37,10 +41,12 @@ class ThrottledStreamIngester extends NOAADataIngester implements FaultTolerantS
     private AtomicLong counter = new AtomicLong(0);
     private long tsLastEmitted = -1;
     private BufferedWriter bufferedWriter;
+    // fault tolerance related configurations
     private Map<String, List<BackupTopicInfo>> topicLocations = new HashMap<>();
     private AtomicLong checkpointCounter = new AtomicLong(0);
     private long checkpointingInterval;
     private long tsLastCheckpoint = -1;
+    private String currentEndpoint;
 
     public ThrottledStreamIngester() {
         super();
@@ -63,18 +69,23 @@ class ThrottledStreamIngester extends NOAADataIngester implements FaultTolerantS
             }
         }
         GeoHashIndexedRecord record = nextRecord();
+        long now = System.currentTimeMillis();
         if (record != null) {
-            long now = System.currentTimeMillis();
-            if (tsLastCheckpoint == -1) {
-                tsLastCheckpoint = now;
-            }
-            if (now - tsLastCheckpoint >= checkpointingInterval) {
-                record.setCheckpointId(checkpointCounter.incrementAndGet());
-                tsLastCheckpoint = now;
-            }
             synchronized (this) {
                 try {
                     writeToStream(Constants.Streams.NOAA_DATA_STREAM, record);
+                    // trigger the downstream check-pointing
+                    if (tsLastCheckpoint == -1) {
+                        tsLastCheckpoint = now;
+                    } else if (now - tsLastCheckpoint >= checkpointingInterval) {
+                        GeoHashIndexedRecord checkpointTrigger = new GeoHashIndexedRecord(checkpointCounter.incrementAndGet(),
+                                getInstanceIdentifier(), currentEndpoint);
+                        writeToStream(Constants.Streams.NOAA_DATA_STREAM, checkpointTrigger);
+                        tsLastCheckpoint = now;
+                        if (logger.isDebugEnabled()) {
+                            logger.debug("Initiated next checkpoint. Checkpoint id: " + checkpointCounter.get());
+                        }
+                    }
                 } catch (StreamingDatasetException e) {
                     logger.error("Faulty downstream. Waiting till switching to a secondary topic.", e);
                     try {
@@ -123,6 +134,8 @@ class ThrottledStreamIngester extends NOAADataIngester implements FaultTolerantS
 
     @Override
     protected void deserializeMemberVariables(FormatReader formatReader) {
+        // instance registration happens here
+        super.deserializeMemberVariables(formatReader);
         // Hack: since Granules does not reinitialize operators after deploying
         // we need a way to read the backup topics from the zk tree just after the deployment.
         // doing it lazily is expensive.
@@ -131,11 +144,14 @@ class ThrottledStreamIngester extends NOAADataIngester implements FaultTolerantS
                 this.checkpointingInterval = ManagedResource.getInstance().getStateReplicationInterval();
                 MembershipTracker.getInstance().registerListener(this);
                 this.topicLocations = populateBackupTopicMap(getInstanceIdentifier(), metadataRegistry);
+                this.currentEndpoint = RivuletUtil.getCtrlEndpoint();
             }
         } catch (NIException e) {
             logger.error("Error acquiring the Resource instance.", e);
         } catch (CommunicationsException e) {
             logger.error("Error registering membership listener.", e);
+        } catch (GranulesConfigurationException e) {
+            logger.error("Error retrieving the current endpoint.", e);
         }
     }
 
