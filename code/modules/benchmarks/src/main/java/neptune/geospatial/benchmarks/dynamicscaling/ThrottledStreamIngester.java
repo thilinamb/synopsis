21dc68458191f9ec4c7c714d0330fc14672b8758
Thilina Buddhika <thilinamb@gmail.com>
2016-04-18 12:58:08 -0600

    Moving FT code out of the dynamic scaling benchmark

--- code/modules/benchmarks/src/main/java/neptune/geospatial/benchmarks/dynamicscaling/ThrottledStreamIngester.java
+++ code/modules/benchmarks/src/main/java/neptune/geospatial/benchmarks/dynamicscaling/ThrottledStreamIngester.java
@@ -1,16 +1,8 @@
 package neptune.geospatial.benchmarks.dynamicscaling;
 
 import com.hazelcast.core.IQueue;
-import ds.funnel.data.format.FormatReader;
-import ds.granules.exception.CommunicationsException;
-import ds.granules.neptune.interfere.core.NIException;
 import ds.granules.streaming.core.exception.StreamingDatasetException;
 import neptune.geospatial.benchmarks.util.SineCurveLoadProfiler;
-import neptune.geospatial.core.resource.ManagedResource;
-import neptune.geospatial.ft.BackupTopicInfo;
-import neptune.geospatial.ft.FaultTolerantStreamBase;
-import neptune.geospatial.ft.zk.MembershipChangeListener;
-import neptune.geospatial.ft.zk.MembershipTracker;
 import neptune.geospatial.graph.Constants;
 import neptune.geospatial.graph.messages.GeoHashIndexedRecord;
 import neptune.geospatial.graph.operators.NOAADataIngester;
@@ -21,15 +13,12 @@ import org.apache.log4j.Logger;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * @author Thilina Buddhika
  */
-public class ThrottledStreamIngester extends NOAADataIngester implements FaultTolerantStreamBase, MembershipChangeListener {
+public class ThrottledStreamIngester extends NOAADataIngester {
 
     private Logger logger = Logger.getLogger(ThrottledStreamIngester.class);
 
@@ -37,7 +26,6 @@ public class ThrottledStreamIngester extends NOAADataIngester implements FaultTo
     private AtomicLong counter = new AtomicLong(0);
     private long tsLastEmitted = -1;
     private BufferedWriter bufferedWriter;
-    private Map<String, List<BackupTopicInfo>> topicLocations = new HashMap<>();
 
     public ThrottledStreamIngester() {
         super();
@@ -108,54 +96,6 @@ public class ThrottledStreamIngester extends NOAADataIngester implements FaultTo
                     true);
         } catch (HazelcastException e) {
             e.printStackTrace();
-        }
-    }
-
-    @Override
-    protected void deserializeMemberVariables(FormatReader formatReader) {
-        // Hack: since Granules does not reinitialize operators after deploying
-        // we need a way to read the backup topics from the zk tree just after the deployment.
-        // doing it lazily is expensive.
-        try {
-            if (ManagedResource.getInstance().isFaultToleranceEnabled()) {
-                MembershipTracker.getInstance().registerListener(this);
-                this.topicLocations = populateBackupTopicMap(getInstanceIdentifier(), metadataRegistry);
-            }
-        } catch (NIException e) {
-            logger.error("Error acquiring the Resource instance.", e);
-        } catch (CommunicationsException e) {
-            logger.error("Error registering membership listener.", e);
-        }
-    }
-
-    @Override
-    public void membershipChanged(List<String> lostMembers) {
-        synchronized (this) {
-            boolean updateTopicLocations = false;
-            for (String lostMember : lostMembers) {
-                if (logger.isDebugEnabled()) {
-                    logger.debug(String.format("[%s] Processing the lost node: %s", getInstanceIdentifier(), lostMember));
-                }
-                // check if a node that hosts an out-going topic has left the cluster
-                if (topicLocations.containsKey(lostMember)) {
-                    // get the list of all topics that was running on the lost node
-                    if (logger.isDebugEnabled()) {
-                        logger.debug(String.format("[%s] Current node is affected by the lost node. Lost node: %s, " +
-                                        "Number of affected topics: %d", getInstanceIdentifier(), lostMember,
-                                topicLocations.get(lostMember).size()));
-                    }
-                    switchToSecondary(topicLocations, lostMember, getInstanceIdentifier(), metadataRegistry);
-                    updateTopicLocations = true;
-                }
-            }
-            // it is required to repopulate the backup nodes list
-            if (updateTopicLocations) {
-                populateBackupTopicMap(this.getInstanceIdentifier(), metadataRegistry);
-                if (logger.isDebugEnabled()) {
-                    logger.debug(String.format("[%s] BackupTopicMap is updated.", getInstanceIdentifier()));
-                }
-            }
-            this.notifyAll();
         }
     }
 }
