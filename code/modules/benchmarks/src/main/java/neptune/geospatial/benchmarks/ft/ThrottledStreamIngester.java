52c6f45450b5b13e0dc72dfc6fff1ea481a90402
Thilina Buddhika <thilinamb@gmail.com>
2016-04-19 22:05:53 -0600

    Replaying failed batches

--- code/modules/benchmarks/src/main/java/neptune/geospatial/benchmarks/ft/ThrottledStreamIngester.java
+++ code/modules/benchmarks/src/main/java/neptune/geospatial/benchmarks/ft/ThrottledStreamIngester.java
@@ -11,6 +11,7 @@ import neptune.geospatial.benchmarks.util.SineCurveLoadProfiler;
 import neptune.geospatial.core.resource.ManagedResource;
 import neptune.geospatial.ft.BackupTopicInfo;
 import neptune.geospatial.ft.FaultTolerantStreamBase;
+import neptune.geospatial.ft.PendingUpstreamCheckpoint;
 import neptune.geospatial.ft.protocol.CheckpointAck;
 import neptune.geospatial.ft.zk.MembershipChangeListener;
 import neptune.geospatial.ft.zk.MembershipTracker;
@@ -25,15 +26,41 @@ import org.apache.log4j.Logger;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * @author Thilina Buddhika
  */
 public class ThrottledStreamIngester extends NOAADataIngester implements FaultTolerantStreamBase, MembershipChangeListener {
+
+    private class CheckpointTimer implements Runnable {
+
+        private final long pendingCheckpointId;
+
+        private CheckpointTimer(long pendingCheckpointId) {
+            this.pendingCheckpointId = pendingCheckpointId;
+        }
+
+        @Override
+        public void run() {
+            synchronized (pendingCheckpoints) {
+                PendingUpstreamCheckpoint pendingCheckpoint = pendingCheckpoints.get(pendingCheckpointId);
+                if (pendingCheckpoint == null) {
+                    if (logger.isDebugEnabled()) {
+                        logger.debug(String.format("[%s]Checkpoint has already been completed. Checkpoint Id: %d",
+                                getInstanceIdentifier(), pendingCheckpointId));
+                    }
+                } else {
+                    replayList.add(pendingCheckpoint);
+                }
+            }
+        }
+    }
 
     private Logger logger = Logger.getLogger(ThrottledStreamIngester.class);
 
@@ -45,8 +72,13 @@ public class ThrottledStreamIngester extends NOAADataIngester implements FaultTo
     private Map<String, List<BackupTopicInfo>> topicLocations = new HashMap<>();
     private AtomicLong checkpointCounter = new AtomicLong(0);
     private long checkpointingInterval;
+    private long checkpointTimeoutPeriod;
     private long tsLastCheckpoint = -1;
     private String currentEndpoint;
+    private final Map<Long, PendingUpstreamCheckpoint> pendingCheckpoints = new HashMap<>();
+    private Queue<GeoHashIndexedRecord> currentBackup = new ConcurrentLinkedQueue<>();
+    private ScheduledExecutorService checkpointMonitor = Executors.newScheduledThreadPool(1);
+    private List<PendingUpstreamCheckpoint> replayList = new ArrayList<>();
 
     public ThrottledStreamIngester() {
         super();
@@ -68,22 +100,62 @@ public class ThrottledStreamIngester extends NOAADataIngester implements FaultTo
 
             }
         }
+        // do the replays first
+        if (!replayList.isEmpty()) {
+            PendingUpstreamCheckpoint checkpoint = replayList.remove(0);
+            Queue<GeoHashIndexedRecord> backup = checkpoint.getBackup();
+            if(logger.isDebugEnabled()){
+                logger.debug(String.format("[%s] Processing replay batch. Checkpoint id: %d, Message Count: %d",
+                        getInstanceIdentifier(), checkpoint.getCheckpointId(), backup.size()));
+            }
+            for (GeoHashIndexedRecord record : backup) {
+                writeToStream(Constants.Streams.NOAA_DATA_STREAM, record);
+                long now = System.currentTimeMillis();
+                if (now - tsLastCheckpoint >= checkpointingInterval) {
+                    long newCheckpointId = checkpointCounter.incrementAndGet();
+                    int outgoingTopicCount = getOutgoingTopicCount();
+                    // create the upstream backup
+                    PendingUpstreamCheckpoint newCheckpoint = new PendingUpstreamCheckpoint(newCheckpointId,
+                            outgoingTopicCount, backup);
+                    pendingCheckpoints.put(newCheckpointId, newCheckpoint);
+                    checkpointMonitor.schedule(new CheckpointTimer(newCheckpointId), checkpointTimeoutPeriod, TimeUnit.MILLISECONDS);
+                    tsLastCheckpoint = now;
+                    if (logger.isDebugEnabled()) {
+                        logger.debug("Initiated next checkpoint. Checkpoint id: " + newCheckpointId);
+                    }
+                }
+            }
+            // we will do a single batch replay in each scheduled iteration
+            return;
+        }
+
         GeoHashIndexedRecord record = nextRecord();
         long now = System.currentTimeMillis();
         if (record != null) {
             synchronized (this) {
                 try {
                     writeToStream(Constants.Streams.NOAA_DATA_STREAM, record);
-                    // trigger the downstream check-pointing
+                    currentBackup.add(record);
+                    // check if its time for the next checkpoint
                     if (tsLastCheckpoint == -1) {
                         tsLastCheckpoint = now;
                     } else if (now - tsLastCheckpoint >= checkpointingInterval) {
-                        GeoHashIndexedRecord checkpointTrigger = new GeoHashIndexedRecord(checkpointCounter.incrementAndGet(),
+                        long checkpointId = checkpointCounter.incrementAndGet();
+                        int outgoingTopicCount = getOutgoingTopicCount();
+                        // create the upstream backup
+                        PendingUpstreamCheckpoint checkpoint = new PendingUpstreamCheckpoint(checkpointId,
+                                outgoingTopicCount, currentBackup);
+                        pendingCheckpoints.put(checkpointId, checkpoint);
+                        checkpointMonitor.schedule(new CheckpointTimer(checkpointId), checkpointTimeoutPeriod,
+                                TimeUnit.MILLISECONDS);
+                        currentBackup = new ConcurrentLinkedQueue<>();
+                        // trigger the downstream check-pointing
+                        GeoHashIndexedRecord checkpointTrigger = new GeoHashIndexedRecord(checkpointId,
                                 getInstanceIdentifier(), currentEndpoint);
                         writeToStream(Constants.Streams.NOAA_DATA_STREAM, checkpointTrigger);
                         tsLastCheckpoint = now;
                         if (logger.isDebugEnabled()) {
-                            logger.debug("Initiated next checkpoint. Checkpoint id: " + checkpointCounter.get());
+                            logger.debug("Initiated next checkpoint. Checkpoint id: " + checkpointId);
                         }
                     }
                 } catch (StreamingDatasetException e) {
@@ -142,6 +214,7 @@ public class ThrottledStreamIngester extends NOAADataIngester implements FaultTo
         try {
             if (ManagedResource.getInstance().isFaultToleranceEnabled()) {
                 this.checkpointingInterval = ManagedResource.getInstance().getStateReplicationInterval();
+                this.checkpointTimeoutPeriod = ManagedResource.getInstance().getCheckpointTimeoutPeriod();
                 MembershipTracker.getInstance().registerListener(this);
                 this.topicLocations = populateBackupTopicMap(getInstanceIdentifier(), metadataRegistry);
                 this.currentEndpoint = RivuletUtil.getCtrlEndpoint();
@@ -186,13 +259,41 @@ public class ThrottledStreamIngester extends NOAADataIngester implements FaultTo
         }
     }
 
+    private int getOutgoingTopicCount() {
+        List<StreamDisseminationMetadata> metadataList = metadataRegistry.get(Constants.Streams.NOAA_DATA_STREAM);
+        int totalCount = 0;
+        for (StreamDisseminationMetadata metadata : metadataList) {
+            totalCount += metadata.topics.length;
+        }
+        return totalCount;
+    }
+
     @Override
     public void handleControlMessage(ControlMessage ctrlMsg) {
-        if(ctrlMsg instanceof CheckpointAck){
-            CheckpointAck ack = (CheckpointAck)ctrlMsg;
-            if(logger.isDebugEnabled()) {
-                logger.debug(String.format("[%s] Received acknowledgments from all child nodes. " +
-                        "Clearup the upstream back for checkpoint: %d",getInstanceIdentifier(), ack.getCheckpointId()));
+        if (ctrlMsg instanceof CheckpointAck) {
+            CheckpointAck ack = (CheckpointAck) ctrlMsg;
+            long checkpointId = ack.getCheckpointId();
+            synchronized (pendingCheckpoints) {
+                PendingUpstreamCheckpoint checkpoint = pendingCheckpoints.get(checkpointId);
+                if (checkpoint != null) {
+                    int pendingAcks = checkpoint.recordChildAck(ack.isSuccess());
+                    boolean success = checkpoint.isSuccess();
+                    if (pendingAcks == 0) {
+                        if (success) {
+                            pendingCheckpoints.remove(checkpointId);
+                            if (logger.isDebugEnabled()) {
+                                logger.debug(String.format("[%s] Received acknowledgments from all child nodes. " +
+                                        "Clearup the upstream back for checkpoint: %d", getInstanceIdentifier(), checkpointId));
+                            }
+                        } else {
+                            replayList.add(checkpoint);
+                            if(logger.isDebugEnabled()){
+                                logger.debug(String.format("[%s] Checkpoint has failed. Adding to replay list. " +
+                                        "Checkpoint Id: %d", getInstanceIdentifier(), checkpoint.getCheckpointId()));
+                            }
+                        }
+                    }
+                }
             }
         }
     }
