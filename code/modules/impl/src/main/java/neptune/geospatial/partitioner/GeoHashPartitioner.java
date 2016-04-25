cb7170707b77e788e2ebf58bf8ee293c8e78bed4
Thilina Buddhika <thilinamb@gmail.com>
2016-04-25 12:24:10 -0600

    Fixing a bug in the Geo-hash partitioner

--- code/modules/impl/src/main/java/neptune/geospatial/partitioner/GeoHashPartitioner.java
+++ code/modules/impl/src/main/java/neptune/geospatial/partitioner/GeoHashPartitioner.java
@@ -22,12 +22,12 @@ public class GeoHashPartitioner implements Partitioner {
         if (ghIndexedRec.getCheckpointId() > -1) {
             return topics;
         } else {
-            int prefix = ghIndexedRec.getPrefixLength();
+            int prefixLen = ghIndexedRec.getPrefixLength() * GeoHash.BITS_PER_CHAR;
             // convert the geohash string into the corresponding bit string
             ArrayList<Boolean> hashInBits = GeoHash.getBits(ghIndexedRec.getGeoHash());
             int sum = 0;
-            for (int i = prefix - 1; i >= 0; i--) {
-                sum += Math.pow(2, prefix - 1 - i) * (hashInBits.get(i) ? 1 : 0);
+            for (int i = prefixLen - 1; i >= 0; i--) {
+                sum += Math.pow(2, prefixLen - 1 - i) * (hashInBits.get(prefixLen - 1 - i) ? 1 : 0);
             }
             Topic topic = topics[sum % topics.length];
             return new Topic[]{topic};
