c804b1de8e90d5b1e60f390ea21729a203d47128
Thilina Buddhika <thilinamb@gmail.com>
2016-04-27 12:30:16 -0600

    Rename StatMessageTypes --> StatConstants

---
+++ code/modules/impl/src/main/java/neptune/geospatial/stat/StatConstants.java
@@ -0,0 +1,27 @@
+package neptune.geospatial.stat;
+
+/**
+ * @author Thilina Buddhika
+ */
+public class StatConstants {
+    public class MessageTypes {
+        public static final int REGISTER = 13021;
+        public static final int PERIODIC_UPDATE = 13022;
+    }
+
+    public class ProcessorTypes {
+        public static final boolean INGESTER = false;
+        public static final boolean PROCESSOR = true;
+    }
+
+    public class RegistryIndices{
+        // processor related metrics
+        public static final int PROC_BACKLOG = 0;
+        public static final int PROC_MEMORY = 1;
+        public static final int PROC_LOCALLY_PROCESSED_PREF_COUNT = 2;
+        public static final int PROC_THROUGHPUT = 3;
+        // ingester related metrics
+        public static final int ING_SENT_MSG_COUNT = 0;
+        public static final int ING_SENT_BYTE_COUNT = 1;
+    }
+}
--- code/modules/impl/src/main/java/neptune/geospatial/stat/StatMessageTypes.java
+++
@@ -1,9 +0,0 @@
-package neptune.geospatial.stat;
-
-/**
- * @author Thilina Buddhika
- */
-public class StatMessageTypes {
-    public static final int REGISTER = 13021;
-    public static final int PERIODIC_UPDATE = 13022;
-}
