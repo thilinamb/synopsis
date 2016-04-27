65f2d2079576ef4ab88bfcb83ac25999acdc2db3
Thilina Buddhika <thilinamb@gmail.com>
2016-04-27 12:50:23 -0600

    Add Stat client

---
+++ code/modules/impl/src/main/java/neptune/geospatial/stat/StatClient.java
@@ -0,0 +1,50 @@
+package neptune.geospatial.stat;
+
+import ds.granules.communication.direct.control.SendUtility;
+import ds.granules.exception.CommunicationsException;
+import ds.granules.exception.GranulesConfigurationException;
+import ds.granules.util.NeptuneRuntime;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+
+/**
+ * A simple client to publish statistics more conveniently.
+ *
+ * @author Thilina Buddhika
+ */
+public class StatClient {
+
+    private static final String STAT_SERVER_ENDPOINT = "stat-server-endpoint";
+
+    private final Logger logger = Logger.getLogger(StatClient.class);
+    private static StatClient instance;
+    private String statServerEndpoint;
+
+    private StatClient(){
+        try {
+            statServerEndpoint = NeptuneRuntime.getInstance().getProperties().getProperty(STAT_SERVER_ENDPOINT);
+        } catch (GranulesConfigurationException e) {
+           logger.error("Error loading startup properties.", e);
+        }
+    }
+
+    public synchronized static StatClient getInstance(){
+        if(instance == null){
+            instance = new StatClient();
+        }
+        return instance;
+    }
+
+    public void publish(StatisticsRecord statRecord){
+        if (statServerEndpoint != null) {
+            try {
+                SendUtility.sendControlMessage(statServerEndpoint, statRecord);
+            } catch (CommunicationsException | IOException e) {
+                logger.error("Error publising stats.", e);
+            }
+        } else {
+            logger.error("Stat Server endpoint is not set.");
+        }
+    }
+}
