6a35e1c41b681ad3a320a8a71d4836c342004719
Thilina Buddhika <thilinamb@gmail.com>
2016-05-02 23:12:23 -0600

    Updating plotting code for better visibility

--- benchmarks/dyn-scaling/dyn-scaling.py
+++ benchmarks/dyn-scaling/dyn-scaling.py
@@ -11,32 +11,36 @@ def main():
     instances = genfromtxt('./data/v3/instance-count.stat', delimiter=',')
     fig,ax2 = plt.subplots(figsize=(8,4))
 
-    leg_thru = plt.plot(thru[:,0], thru[:,1], color='darkblue', label='Data Ingestion Rate')
-    pylab.ylabel('Data Ingestion Rate(Msgs/s)', fontsize=10)
+    colors = ['#332288', '#88CCEE', '#44AA99', '#117733', '#999933', '#DDCC77', '#CC6677', '#882255', '#AA4499']
+    col_1 = colors[0]
+    col_2 = colors[7]
+
+    leg_thru = plt.plot(thru[:,0], thru[:,1], color=col_1, dashes=(1,2), lw=1.3, label='Data Ingestion Rate',)
+    pylab.ylabel('Data Ingestion Rate(Msgs/s)', fontsize=13)
     pylab.ylim((0,400))
-    ax2.tick_params(axis='y', colors='darkblue')
-    ax2.yaxis.label.set_color('darkblue')
+    ax2.tick_params(axis='y', colors=col_1)
+    ax2.yaxis.label.set_color(col_1)
     ax2.xaxis.set_major_formatter(NullFormatter())
-    pylab.xlabel('Time', fontsize=10)
+    pylab.xlabel('Time', fontsize=13)
 
     ax = ax2.twinx()
-    leg_comp = ax.step(instances[:,0], instances[:,1], color='magenta', label='Number of Sketch Instances')
-    ax.tick_params(axis='y', colors='magenta')
-    ax.yaxis.label.set_color('magenta')
+    leg_comp = ax.step(instances[:,0], instances[:,1], lw=1.5, color=col_2, label='Number of Sketchlets')
+    ax.tick_params(axis='y', colors=col_2)
+    ax.yaxis.label.set_color(col_2)
     pylab.ylim((0,1100))
 
-    pylab.xlabel('Time', fontsize=10)
-    pylab.ylabel('Number of Sketch Instances', fontsize=10)
-    plt.tick_params(axis='x', which='major', labelsize=10)
-    plt.tick_params(axis='y', which='major', labelsize=10)
-    plt.tick_params(axis='both', which='minor', labelsize=9)
-    
-    lns = leg_comp + leg_thru
+    pylab.xlabel('Time', fontsize=14)
+    pylab.ylabel('Number of Sketchlets', fontsize=13)
+    plt.tick_params(axis='x', which='major', labelsize=12)
+    plt.tick_params(axis='y', which='major', labelsize=12)
+    plt.tick_params(axis='both', which='minor', labelsize=10)
+
+    lns = leg_thru + leg_comp
     labs = [l.get_label() for l in lns]
-    leg = ax.legend(lns, labs, fontsize=9, ncol=2)
+    leg = ax.legend(lns, labs, fontsize=12, ncol=2, bbox_to_anchor=(0.5, -0.20), loc=8, borderaxespad=0., handlelength=3)
     leg.get_frame().set_linewidth(0.1)
     pylab.tight_layout()
-    plt.savefig('figs/dyn_scaling_v3.pdf', dpi=300)
+    plt.savefig('figs/dyn_scaling_v4.pdf', dpi=300, bbox_inches='tight')
     plt.close()
 
 if __name__ == '__main__':
--- benchmarks/stability/stability.py
+++ benchmarks/stability/stability.py
@@ -12,7 +12,7 @@ def parse_scaling_activities(dataset):
     activities = []
     for i in range(0, count, 2):
         activities.append([dataset[i,1], dataset[i+1,1]])
-    
+
     return activities
 
 def main():
@@ -27,16 +27,22 @@ def main():
     scale_out_activities = parse_scaling_activities(scale_out)
     scale_in_activities = parse_scaling_activities(scale_in)
 
-    fig,ax2 = plt.subplots(figsize=(8,4))
-    leg_backlog = plt.plot(backlog[:,0], backlog[:,1], lw=0.8, label='Backlog Size', color='darkred')
+    colors = ['#332288', '#88CCEE', '#44AA99', '#117733', '#999933', '#DDCC77', '#CC6677', '#882255', '#AA4499']
+    col_1= colors[0]
+    col_2= 'black'
+    col_3 = colors[3]
+    col_4 = colors[7]
+
+    fig,ax2 = plt.subplots(figsize=(8.5,4))
+    leg_backlog = plt.plot(backlog[:,0], backlog[:,1], lw=0.8, label='Backlog Size', color=col_1)
     xmin, xmax = ax2.get_xlim()
-    leg_threshold = plt.plot((xmin, xmax), (20, 20), '-.', color='dimgray', label='Scale Out Threshold')
+    leg_threshold = plt.plot((xmin, xmax), (20, 20), '-.', lw=1.2, color=col_2, label='Scale Out Threshold')
     ax2.xaxis.set_major_formatter(NullFormatter())
 
-    pylab.ylabel('Input Rate(Messages/s)', fontsize=10)
-    pylab.xlabel('Time', fontsize=10)
+    pylab.ylabel('Input Rate(Messages/s)', fontsize=12)
+    pylab.xlabel('Time', fontsize=12)
 
-    leg_input_rate = plt.plot(input_rates[:,0], input_rates[:,1]/2527, color='darkblue', label='Input Rate')
+    leg_input_rate = plt.plot(input_rates[:,0], input_rates[:,1]/2527 ,dashes=(3,1), color=col_3, label='Input Rate')
 
     for entry in scale_out_activities:
         rect_scale_out = ax2.add_patch(patches.Rectangle((entry[0],0), (entry[1] - entry[0]), 350, alpha=0.4, color='orange', label='Scale Out'))
@@ -44,17 +50,17 @@ def main():
     for entry in scale_in_activities:
         rect_scale_in = ax2.add_patch(patches.Rectangle((entry[0],0), (entry[1] - entry[0]), 350, alpha=0.4, color='deepskyblue', label='Scale In'))
 
-    leg_thru = plt.plot(throughput[:,0], throughput[:,1], color='magenta', label='Throughput')
+    leg_thru = plt.plot(throughput[:,0], throughput[:,1], color=col_4, lw=1.3, label='Throughput')
 
     lns = leg_backlog + leg_threshold + leg_input_rate + leg_thru
     lns = lns + [rect_scale_out, rect_scale_in]
     labs = [l.get_label() for l in lns]
-    leg = ax2.legend(lns, labs, fontsize=9, ncol=6, bbox_to_anchor=(0.5, -0.16), loc=8, borderaxespad=0., handlelength=3)
+    leg = ax2.legend(lns, labs, fontsize=11, ncol=4, bbox_to_anchor=(0.5, -0.26), loc=8, borderaxespad=0., handlelength=3)
     leg.get_frame().set_linewidth(0.1)
     pylab.tight_layout()
 
     plt.savefig('./figs/stability_{}.pdf'.format(version), dpi=300, bbox_extra_artists=(leg,), bbox_inches='tight')
     plt.close()
-    
+
 if __name__ == '__main__':
     main()
