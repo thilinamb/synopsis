997f80b93849033ebc48807d9b410038f6556baf
Thilina Buddhika <thilinamb@gmail.com>
2016-10-10 14:20:50 -0600

    Recording total ingested byte count

--- code/modules/stats/src/main/java/synopsis/statserver/processors/CumulProcessedMsgCountProcessor.java
+++ code/modules/stats/src/main/java/synopsis/statserver/processors/CumulProcessedMsgCountProcessor.java
@@ -24,12 +24,16 @@ public class CumulProcessedMsgCountProcessor implements MetricProcessor {
     @Override
     public void process(Map<String, double[]> metricData, long ts, BufferedWriter buffW) throws IOException {
         double totalProcessed = 0.0;
+        double totalProcessedData = 0.0;
         for (double[] metrics : metricData.values()) {
             double val = metrics[StatConstants.RegistryIndices.ING_SENT_MSG_COUNT];
             if (val != -1) {
                 totalProcessed += val;
             }
+            if(metrics[StatConstants.RegistryIndices.ING_SENT_BYTE_COUNT] != -1){
+                totalProcessedData += metrics[StatConstants.RegistryIndices.ING_SENT_BYTE_COUNT];
+            }
         }
-        buffW.write(ts + "," + totalProcessed);
+        buffW.write(ts + "," + totalProcessed + "," + totalProcessedData);
     }
 }
