ef7ec4a76d0a68bd3adb4da34bc0f9c095903d9d
Thilina Buddhika <thilinamb@gmail.com>
2016-10-10 17:40:48 -0600

    Initial commit of the Synopsis client

---
+++ code/modules/client/src/main/java/synopsis.client/Main.java
@@ -0,0 +1,11 @@
+package synopsis.client;
+
+/**
+ * Main class of the Synopsis Client
+ * @author Thilina Buddhika
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Client is running!");
+    }
+}
