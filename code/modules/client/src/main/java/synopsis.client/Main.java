7244ebb4cc40713cc294b476d9a74ad4ac701da8
Thilina Buddhika <thilinamb@gmail.com>
2016-10-11 17:16:29 -0600

    Driver code for client for testing

--- code/modules/client/src/main/java/synopsis.client/Main.java
+++ code/modules/client/src/main/java/synopsis.client/Main.java
@@ -1,11 +1,41 @@
 package synopsis.client;
 
+import org.apache.log4j.Logger;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
 /**
  * Main class of the Synopsis Client
+ *
  * @author Thilina Buddhika
  */
 public class Main {
+
+    private static Logger LOGGER = Logger.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Client is running!");
+        try {
+            if (args.length < 2) {
+                System.err.println("Usage: <path-to-config> <port>");
+                System.exit(-1);
+            }
+            String configFilePath = args[0];
+            int port = Integer.parseInt(args[1]);
+            LOGGER.info("Using the config file: " + configFilePath);
+            LOGGER.info("Using the client port: " + port);
+            Properties properties = new Properties();
+            properties.load(new FileInputStream(configFilePath));
+            Client client = new Client(properties, port);
+            new CountDownLatch(1).await();
+        } catch (IOException e) {
+            LOGGER.error("Error when populating the properties.", e);
+        } catch (ClientException e) {
+            LOGGER.error("Error initializing Synopsis client.", e);
+        } catch (InterruptedException ignore) {
+
+        }
     }
 }
