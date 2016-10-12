eab75d754a589e3a9bfc081ca0f0f8cbdc4f65ce
Thilina Buddhika <thilinamb@gmail.com>
2016-10-12 16:34:57 -0600

    Sending query response back to the client

---
+++ code/modules/impl/src/main/java/neptune/geospatial/core/protocol/msg/client/TargetQueryResponse.java
@@ -0,0 +1,65 @@
+package neptune.geospatial.core.protocol.msg.client;
+
+import ds.granules.communication.direct.control.ControlMessage;
+import neptune.geospatial.core.protocol.ProtocolTypes;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+/**
+ * @author Thilina Buddhika
+ */
+public class TargetQueryResponse extends ControlMessage {
+
+    private int queryId;
+    private String compId;
+    private byte[] response;
+    private long queryEvalTime;
+
+    public TargetQueryResponse() {
+        super(ProtocolTypes.TARGET_QUERY_RESP);
+    }
+
+    public TargetQueryResponse(int queryId, String compId, byte[] response, long queryEvalTime) {
+        super(ProtocolTypes.TARGET_QUERY_RESP);
+        this.queryId = queryId;
+        this.compId = compId;
+        this.response = response;
+        this.queryEvalTime = queryEvalTime;
+    }
+
+    @Override
+    public void readValues(DataInputStream dataInputStream) throws IOException {
+        this.queryId = dataInputStream.readInt();
+        this.compId = dataInputStream.readUTF();
+        this.response = new byte[dataInputStream.readInt()];
+        dataInputStream.readFully(this.response);
+        this.queryEvalTime = dataInputStream.readLong();
+    }
+
+    @Override
+    public void writeValues(DataOutputStream dataOutputStream) throws IOException {
+        dataOutputStream.writeInt(this.queryId);
+        dataOutputStream.writeUTF(this.compId);
+        dataOutputStream.writeInt(this.response.length);
+        dataOutputStream.write(this.response);
+        dataOutputStream.writeLong(this.queryEvalTime);
+    }
+
+    public int getQueryId() {
+        return queryId;
+    }
+
+    public String getCompId() {
+        return compId;
+    }
+
+    public byte[] getResponse() {
+        return response;
+    }
+
+    public long getQueryEvalTime() {
+        return queryEvalTime;
+    }
+}
--- code/modules/impl/src/main/java/neptune/geospatial/core/protocol/processors/client/TargetedQueryProcessor.java
+++ code/modules/impl/src/main/java/neptune/geospatial/core/protocol/processors/client/TargetedQueryProcessor.java
@@ -1,11 +1,16 @@
 package neptune.geospatial.core.protocol.processors.client;
 
 import ds.granules.communication.direct.control.ControlMessage;
+import ds.granules.communication.direct.control.SendUtility;
+import ds.granules.exception.CommunicationsException;
 import neptune.geospatial.core.computations.AbstractGeoSpatialStreamProcessor;
 import neptune.geospatial.core.computations.scalingctxt.ScalingContext;
+import neptune.geospatial.core.protocol.msg.client.TargetQueryResponse;
 import neptune.geospatial.core.protocol.msg.client.TargetedQueryRequest;
 import neptune.geospatial.core.protocol.processors.ProtocolProcessor;
 import org.apache.log4j.Logger;
+
+import java.io.IOException;
 
 /**
  * @author Thilina Buddhika
@@ -18,7 +23,17 @@ public class TargetedQueryProcessor implements ProtocolProcessor {
     public void process(ControlMessage ctrlMsg, ScalingContext scalingContext,
                         AbstractGeoSpatialStreamProcessor streamProcessor) {
         TargetedQueryRequest queryReq = (TargetedQueryRequest) ctrlMsg;
+        long startTime = System.nanoTime();
         byte[] response = streamProcessor.query(queryReq.getQuery());
-        // todo: send it back to the client
+        long endTime = System.nanoTime();
+        logger.info(String.format("[%s] Query evaluated. Query id: %d, Eval. time: %d",
+                streamProcessor.getInstanceIdentifier(), queryReq.getQueryId(), (endTime - startTime)));
+        TargetQueryResponse queryResponse = new TargetQueryResponse(queryReq.getQueryId(),
+                streamProcessor.getInstanceIdentifier(), response, (endTime - startTime));
+        try {
+            SendUtility.sendControlMessage(queryReq.getClientAddr(), queryResponse);
+        } catch (CommunicationsException | IOException e) {
+            logger.error("Error sending back the query response to the client. Query id: " + queryReq.getQueryId(), e);
+        }
     }
 }
