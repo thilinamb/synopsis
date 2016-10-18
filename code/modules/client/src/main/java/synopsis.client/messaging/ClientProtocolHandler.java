1ea255b68fe33d90c62d4590dc3ad9f22a1e1a09
Thilina Buddhika <thilinamb@gmail.com>
2016-10-18 17:40:39 -0600

    Updated client protocol handler to support persistence ack and persistence responses

--- code/modules/client/src/main/java/synopsis.client/messaging/ClientProtocolHandler.java
+++ code/modules/client/src/main/java/synopsis.client/messaging/ClientProtocolHandler.java
@@ -4,8 +4,11 @@ import ds.granules.communication.direct.control.ControlMessage;
 import neptune.geospatial.core.protocol.AbstractProtocolHandler;
 import neptune.geospatial.core.protocol.ProtocolTypes;
 import neptune.geospatial.core.protocol.msg.client.ClientQueryResponse;
+import neptune.geospatial.core.protocol.msg.client.PersistStateAck;
+import neptune.geospatial.core.protocol.msg.client.PersistStateResponse;
 import neptune.geospatial.core.protocol.msg.client.TargetQueryResponse;
 import org.apache.log4j.Logger;
+import synopsis.client.persistence.PersistenceManager;
 import synopsis.client.query.QueryManager;
 
 import java.util.concurrent.CountDownLatch;
@@ -18,24 +21,33 @@ public class ClientProtocolHandler extends AbstractProtocolHandler {
     private final Logger logger = Logger.getLogger(ClientProtocolHandler.class);
     private final CountDownLatch startedFlag;
     private final QueryManager queryManager;
+    private final PersistenceManager persistenceManager;
 
     public ClientProtocolHandler(CountDownLatch startedFlag, QueryManager queryManager) {
         this.startedFlag = startedFlag;
         this.queryManager = queryManager;
+        this.persistenceManager = PersistenceManager.getInstance();
     }
+
 
     @Override
     public void handle(ControlMessage ctrlMsg) {
         int type = ctrlMsg.getMessageType();
-        if(logger.isDebugEnabled()) {
+        if (logger.isDebugEnabled()) {
             logger.debug("Dispatching ctrl message. Type: " + type);
         }
-        switch (type){
+        switch (type) {
             case ProtocolTypes.CLIENT_QUERY_RESP:
                 queryManager.handleClientQueryResponse((ClientQueryResponse) ctrlMsg);
                 break;
             case ProtocolTypes.TARGET_QUERY_RESP:
                 queryManager.handleTargetQueryResponse((TargetQueryResponse) ctrlMsg);
+                break;
+            case ProtocolTypes.PERSIST_STATE_ACK:
+                persistenceManager.handlePersistenceAck((PersistStateAck) ctrlMsg);
+                break;
+            case ProtocolTypes.PERSIST_STATE_RESP:
+                persistenceManager.handlePersistStateResponse((PersistStateResponse) ctrlMsg);
                 break;
             default:
                 logger.warn("Unsupported control message. Type: " + type);
