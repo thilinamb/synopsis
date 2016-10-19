55101dd03cbc0f7810c4b1b81a690595fb60e75a
Matthew Malensek <matt@malensek.net>
2016-10-19 17:52:05 -0600

    Add class to contain query info

---
+++ code/modules/impl/src/main/java/neptune/geospatial/graph/operators/QueryWrapper.java
@@ -0,0 +1,11 @@
+package neptune.geospatial.graph.operators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QueryWrapper {
+
+    public byte[] payload;
+    public List<String> geohashes = new ArrayList<>();
+
+}
