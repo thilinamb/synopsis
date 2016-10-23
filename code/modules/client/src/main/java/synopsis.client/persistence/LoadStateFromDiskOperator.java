5f856ae50f993f1b6658902d1babec965e850c59
Thilina Buddhika <thilinamb@gmail.com>
2016-10-23 17:16:15 -0600

    Initial version of the operator that can load state from the disk

---
+++ code/modules/client/src/main/java/synopsis.client/persistence/LoadStateFromDiskOperator.java
@@ -0,0 +1,43 @@
+package synopsis.client.persistence;
+
+import ds.funnel.data.format.FormatReader;
+import ds.funnel.data.format.FormatWriter;
+import ds.granules.neptune.interfere.core.NIException;
+import neptune.geospatial.core.resource.ManagedResource;
+import neptune.geospatial.graph.operators.SketchProcessor;
+import org.apache.log4j.Logger;
+
+/**
+ * @author Thilina Buddhika
+ */
+public class LoadStateFromDiskOperator extends SketchProcessor {
+
+    private String serializedStateLocation;
+    private Logger logger = Logger.getLogger(LoadStateFromDiskOperator.class);
+
+    public LoadStateFromDiskOperator() {
+        super();
+    }
+
+    public void setSerializedStateLocation(String serializedStateLocation) {
+        this.serializedStateLocation = serializedStateLocation;
+    }
+
+    @Override
+    protected void deserializeMemberVariables(FormatReader formatReader) {
+        super.deserializeMemberVariables(formatReader);
+        this.serializedStateLocation = formatReader.readString();
+        try {
+            ManagedResource.getInstance().registerStreamProcessor(this);
+            logger.info("Serialized location: " + this.serializedStateLocation);
+        } catch (NIException e) {
+            logger.error("Error registering at Managed Resource.", e);
+        }
+    }
+
+    @Override
+    protected void serializedMemberVariables(FormatWriter formatWriter) {
+        super.serializedMemberVariables(formatWriter);
+        formatWriter.writeString(this.serializedStateLocation);
+    }
+}
